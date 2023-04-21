1b92285f12e06b60d746a40ebbdbc8731bfdc894
Paul Teng <plankp@outlook.com>
2023-04-21 05:40:40 -0400

    Maintain two environments when reindexing
    One for value, one for continuations

--- PassReindex.ml
+++ PassReindex.ml
@@ -2,71 +2,70 @@ open Ast
 
 module M = Map.Make (Int)
 
-let rec reindex' r s id =
-  match !r with
-    | Module _ ->
-      failwith "INVALID NESTED MODULE"
+let rec reindex' r sv sk id = match !r with
+  | Module _ ->
+    failwith "INVALID NESTED MODULE"
 
-    | Export xs ->
-      r := Export (List.map (fun (n, i) -> (n, M.find i s)) xs);
-      id
+  | Export xs ->
+    r := Export (List.map (fun (n, i) -> (n, M.find i sv)) xs);
+    id
 
-    | LetCont (j, args, body, e) ->
-      let j, id, s = id, id + 1, M.add j id s in
-      let args, id, s' = List.fold_left (fun (xs, id, s') a ->
-        (id :: xs, id + 1, M.add a id s')) ([], id, s) args in
+  | LetCont (j, args, body, e) ->
+    let j, id, sk = id, id + 1, M.add j id sk in
+    let args, id, sv' = List.fold_left (fun (xs, id, sv') a ->
+      (id :: xs, id + 1, M.add a id sv')) ([], id, sv) args in
 
-      r := LetCont (j, List.rev args, body, e);
-      id |> reindex' body s' |> reindex' e s
+    r := LetCont (j, List.rev args, body, e);
+    id |> reindex' body sv' sk |> reindex' e sv sk
 
-    | LetFun (f, args, k, body, e) ->
-      (* LetFun is NOT recursive, so don't augment s here *)
-      let f', id = id, id + 1 in
-      (* the args only affect the binder body *)
-      let args, id, s' = List.fold_left (fun (xs, id, s') a ->
-        (id :: xs, id + 1, M.add a id s')) ([], id, s) args in
-      let k, id, s' = id, id + 1, M.add k id s' in
+  | LetFun (f, args, k, body, e) ->
+    (* LetFun is NOT recursive, so don't augment sv here *)
+    let f', id = id, id + 1 in
+    (* the args only affect the binder body *)
+    let args, id, sv' = List.fold_left (fun (xs, id, sv') a ->
+      (id :: xs, id + 1, M.add a id sv')) ([], id, sv) args in
+    let k, id, sk' = id, id + 1, M.add k id sk in
 
-      r := LetFun (f', List.rev args, k, body, e);
-      id |> reindex' body s' |> reindex' e (M.add f f' s)
+    r := LetFun (f', List.rev args, k, body, e);
+    id |> reindex' body sv' sk' |> reindex' e (M.add f f' sv) sk
 
-    | LetRec (bs, e) ->
-      (* we need to augment s with all recursive binders first *)
-      let bs, id, s = List.fold_left (fun (xs, id, s) (f, args, k, body) ->
-        ((id, args, k, body) :: xs, id + 1, M.add f id s)) ([], id, s) bs in
+  | LetRec (bs, e) ->
+    (* we need to augment s with all recursive binders first *)
+    let bs, id, sv = List.fold_left (fun (xs, id, sv) (f, args, k, body) ->
+      ((id, args, k, body) :: xs, id + 1, M.add f id sv)) ([], id, sv) bs in
 
-      (* then reindex binder body *)
-      let bs, id = List.fold_left (fun (xs, id) (f, args, k, body) ->
-        let args, id, s = List.fold_left (fun (xs, id, s) a ->
-          (id :: xs, id + 1, M.add a id s)) ([], id, s) args in
-        let k, id, s = id, id + 1, M.add k id s in
-        ((f, List.rev args, k, body) :: xs, reindex' body s id)) ([], id) bs in
+    (* then reindex binder body *)
+    let bs, id = List.fold_left (fun (xs, id) (f, args, k, body) ->
+      let args, id, sv = List.fold_left (fun (xs, id, sv) a ->
+        (id :: xs, id + 1, M.add a id sv)) ([], id, sv) args in
+      let k, id, sk = id, id + 1, M.add k id sk in
+      ((f, List.rev args, k, body) :: xs, reindex' body sv sk id)) ([], id) bs in
 
-      r := LetRec (bs, e);
-      reindex' e s id
+    r := LetRec (bs, e);
+    reindex' e sv sk id
 
-    | Jmp (k, xs) ->
-      r := Jmp (M.find k s, List.map (fun i -> M.find i s) xs);
-      id
+  | Jmp (k, xs) ->
+    r := Jmp (M.find k sk, List.map (fun i -> M.find i sv) xs);
+    id
 
-    | App (f, xs, k) ->
-      r := App (M.find f s, List.map (fun i -> M.find i s) xs, M.find k s);
-      id
+  | App (f, xs, k) ->
+    r := App (M.find f sv, List.map (fun i -> M.find i sv) xs, M.find k sk);
+    id
 
-    | LetPack (v, elts, e) ->
-      let elts = List.map (fun i -> M.find i s) elts in
-      let v, id, s = id, id + 1, M.add v id s in
+  | LetPack (v, elts, e) ->
+    let elts = List.map (fun i -> M.find i sv) elts in
+    let v, id, sv = id, id + 1, M.add v id sv in
 
-      r := LetPack (v, elts, e);
-      reindex' e s id
+    r := LetPack (v, elts, e);
+    reindex' e sv sk id
 
-    | LetProj (v, i, p, e) ->
-      let p = M.find p s in
-      let v, id, s = id, id + 1, M.add v id s in
+  | LetProj (v, i, p, e) ->
+    let p = M.find p sv in
+    let v, id, sv = id, id + 1, M.add v id sv in
 
-      r := LetProj (v, i, p, e);
-      reindex' e s id
+    r := LetProj (v, i, p, e);
+    reindex' e sv sk id
 
 let reindex = function
-  | Module r -> reindex' r M.empty 0
+  | Module r -> reindex' r M.empty M.empty 0
   | _ -> failwith "INVALID TERM ANCHOR"
